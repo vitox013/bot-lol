17384c8442c24f8b06a9deb1b7fc30182b0ce785
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2022-06-08 01:39:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ def verificaTela():
     button_pos = pyautogui.locateOnScreen('button.png', confidence=0.8)
     if button_pos != None:
         click(button_pos.left + 80, button_pos.top + 20)
+        print("\tPartida encontrada")
         return True
     return False
 
@@ -21,10 +22,14 @@ def verificaInicio():
     
 
 def main():
+    print("\n\n\tBot criado por Viteeeera (https://github.com/vitox013/bot-lol)\n\n\tVá lá no banheiro que o bot aceitará sozinho caso ache uma partida\n\n")
     while verificaInicio():
         time.sleep(2)
         verificaTela()
-        print('Aguardando para aceitar')   
+    print("\n\n\tPartida iniciada, Boa partida!")
+    print("\n\n\tApp será fechado!")
+    time.sleep(5)
+   
 
 main()
 
