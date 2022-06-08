2a88719960a5d7aac8db531bd2c53dc0ff7f30af
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2022-06-07 23:11:10 -0300

    aparentemente concluido

--- bot.py
+++ bot.py
@@ -1,2 +1,29 @@
 import pyautogui
 import time
+
+def click(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click
+
+
+def verificaTela():
+    button_pos = pyautogui.locateOnScreen('botaoAceitarOk.png', confidence=0.8)
+    if button_pos != None:
+        click(button_pos.left + 80, button_pos.top + 20)
+        return True
+    return False
+
+def verificaInicio():
+    image_pos = pyautogui.locateOnScreen('verificaInicio.png', confidence=0.9)
+    if image_pos != None:
+        return False
+    return True
+    
+
+def main():
+    while verificaInicio():
+        verificaTela()
+        print('Aguardando para aceitar')   
+
+main()
+
